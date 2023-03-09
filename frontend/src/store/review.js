e235d23e3a9e01bf0472de36357506dfecdeb31a
rexqu <junjiequ123@gmail.com>
2023-03-08 21:54:01 -0500

    fix review reducer

--- frontend/src/store/review.js
+++ frontend/src/store/review.js
@@ -1,38 +1,39 @@
 import csrfFetch from "./csrf.js";
 
-export const ADD_REVIEW = 'review/addReview';
-
+export const ADD_REVIEW = "review/addReview";
 
 const addReview = (review) => ({
-    type: ADD_REVIEW,
-    payload: review,
+  type: ADD_REVIEW,
+  payload: review,
 });
 
 export const createReview = (review) => async (dispatch) => {
-    const response = await csrfFetch(`/api/reviews/`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(review)
-    });
-    const data = await response.json();
-    dispatch(addReview(data));
-}
+  const { content, id, rating, business_id, author_id } = review;
+  const response = await csrfFetch("/api/reviews", {
+    method: "POST",
+    body: JSON.stringify({
+      id,
+      content,
+      rating,
+      business_id,
+      author_id,
+    }),
+  });
 
-
-const reviewReducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD_REVIEW:
-            const review = action.payload;
-            return { ...state, reviews: [...state, review] };
-        default:
-            return state;
-    }
+  const data = await response.json();
+  dispatch(addReview(data));
+  return response;
 };
 
+const reviewReducer = (state = [], action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case ADD_REVIEW:
+      const review = action.payload;
+      return { ...state, reviews: [...state, review] };
+    default:
+      return state;
+  }
+};
 
-
-
-
-export default reviewReducer
+export default reviewReducer;
